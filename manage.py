b644371837cbd3c08c5d2d03c2e005a5accec93a
Kristen Chandler <krismchandler@gmail.com>
2022-11-15 20:59:10 -0600

    chapter 2 models finished

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    import pdb; pdb.set_trace()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'levelup.settings')
     try:
         from django.core.management import execute_from_command_line
